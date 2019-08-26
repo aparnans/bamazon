bcf2af5e6d635df64e3a5e9b1478fb4088a11e9a
aparnans <aparna_ns@yahoo.com>
2019-08-26 00:33:08 -0400

    Added gitignore

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -8,80 +8,90 @@ var mysql = require("mysql");
 // Global variables to hold 
 var gUser = "";
 var gPassword = "";
+var guserVerified = false;
+var departmentList = [];
+var gConnection;
 
-var connection = mysql.createConnection({
-   host: "localhost",
-   // Your port; if not 3306
-   port: 3306,
-   // Your username
-   user: "root",
-   // Your password
-   password: "MyPwd#1",
-   database: "bamazom"
-});
+function connectDB() {
+    gConnection = mysql.createConnection({
+        host: "localhost",
+        // Your port; if not 3306
+        port: 3306,
+        // Your username
+        user: "root",
+        // Your password
+        password: "MyPwd#1",
+        database: "bamazom"
+    });
+};
 
 function acceptPassword() {
-   console.log('in accept password again');
-   inquirer.prompt([
-       {
-           type: "password",
-           name: "myPassword",
-           message: "The password does not match, Please enter it again: "
-       }
-   ]).then(function (res) {
-       gPassword=res.myPassword;
-       if (isPasswordValid(gPassword, gUser)) {
-           connection.end();
-           return true;
-       }
-       else {
-           acceptPassword();
-       };
-   })
+    console.log('in accept password again');
+    inquirer.prompt([
+        {
+            type: "password",
+            name: "myPassword",
+            message: "The password does not match, Please enter it again: "
+        }
+    ]).then(function (res) {
+        if (isPasswordValid(gPassword, res.myPassword)) {
+            console.log('Returning after passwords match in acceptPassword');
+            return true;
+        }
+        else {
+            acceptPassword();
+        };
+    })
 }
 function acceptUserName() {
-   inquirer.prompt([
-       {
-           type: "input",
-           name: "userName",
-           message: "Enter your user name: "
-       },
-       {
-           type: "password",
-           name: "myPassword",
-           message: "Enter your password: "
-       }
-   ]).then(function (user) {
-       // get the password of the user from the DB
-       connection.connect(function (err) {
-           if (err) throw err;
-           var query = connection.query(
-               "Select password from users where user_name = ?", [user.userName],
-               function (err, res) {
-                   if (err) throw err;
-                   gPassword = user.myPassword;
-                   gUser = user.userName;
-                   if (isPasswordValid(gUser,gPassword)) {
-                       connection.end();
-                       return true;
-                   }
-                   else {
-                       acceptPassword();
-                   };
-               }
-           )
-           console.log(query.sql);
-       })
-   });
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "userName",
+            message: "Enter your user name: "
+        },
+        {
+            type: "password",
+            name: "myPassword",
+            message: "Enter your password: "
+        }
+    ]).then(function (user) {
+        // get the password of the user from the DB
+        gConnection.connect(function (err) {
+            if (err) throw err;
+            var query = gConnection.query(
+                "Select password from users where username = ?", [user.userName],
+                function (err, res) {
+                    if (err) throw err;
+                    gPassword = res[0].password;
+                    gUser = user.userName;
+                    if (isPasswordValid(gPassword, user.myPassword)) {
+                        console.log("password is valid");
+                        guserVerified = true;
+                        console.log("Calling acceptDepartment");
+                        acceptDepartment();
+                    }
+                    else {
+                        acceptPassword();
+                    };
+                }
+            )
+            return true;
+        })
+    });
+
+
 }
 function isPasswordValid(dbPassword, password) {
-   if (dbPassword != password) {
-       return false;
-   } else {
-       return true;
-   }
+    console.log('in isPasswordValid : dbPassword : password : ', dbPassword, password);
+    if (dbPassword != password) {
+        return false;
+    } else {
+        guserVerified=true;
+        console.log('passwords match');
+        return true;
+    }
 }
-acceptUserName();
 
 // user prompts
 
@@ -89,23 +99,38 @@ acceptUserName();
 // show item options
 // show options for sizes
 
-
-
-
-
-
 function acceptDepartment() {
-    inquirer
-    .prompt({
-      name: "department",
-      type: "list",
-      message: "Please select the department to shop from:",
-      choices: ["Womens Dresses", "Skirts", "Ments T-shirts", "Ments Belts"]
+    console.log('in accept department');
+    //use the connection to select department names
+    gConnection.query('SELECT distinct department_name from products', function (error, res) {
+        console.log('After select ',res[0].department_name);
+        if (error) throw error;
+        console.log(res[1]);
+        for (var i = 0; i < res.length; i++) {
+            departmentList.push(res[i].department_name);
+        }
     })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-        console.log(answer);
-    });
- }
+    inquirer
+        .prompt({
+            name: "department",
+            type: "list",
+            message: "Please select the department to shop from:",
+            choices: departmentList//["Womens Dresses", "Skirts", "Ments T-shirts", "Ments Belts"]
+        })
+        .then(function (answer) {
+            // based on their answer, show the list of items to choose from 
+            console.log(answer);
+        });
+    return true;
+}
 
- acceptDepartment();
+function start() {
+    connectDB();
+    acceptUserName();
+    
+    
+    return true;
+
+}
+
+start();
