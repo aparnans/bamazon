f65d95f8ba3f6b51b637c6cce1d6db1ba5ca8d40
aparnans <aparna_ns@yahoo.com>
2019-08-28 22:17:09 -0400

    added code to sell items

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,16 +1,16 @@
 //bamazonCustomer.js
 
-
+//Global variables
 var inquirer = require("inquirer");
 var mysql = require("mysql");
 
-// Accept user login
-// Global variables to hold 
 var gUser = "";
 var gPassword = "";
 var guserVerified = false;
 var departmentList = [];
 var gConnection;
+var sizeArray =[];
+var productArray = [];
 
 function connectDB() {
     gConnection = mysql.createConnection({
@@ -24,7 +24,7 @@ function connectDB() {
         database: "bamazom"
     });
 };
-
+// This function is to accept password again if the earlier attempt failed 
 function acceptPassword() {
     console.log('in accept password again');
     inquirer.prompt([
@@ -42,7 +42,9 @@ function acceptPassword() {
             acceptPassword();
         };
     })
+    return true;
 }
+// This function accepts user name and password to use for shopping
 function acceptUserName() {
     inquirer.prompt([
         {
@@ -68,69 +70,206 @@ function acceptUserName() {
                     if (isPasswordValid(gPassword, user.myPassword)) {
                         console.log("password is valid");
                         guserVerified = true;
-                        console.log("Calling acceptDepartment");
-                        acceptDepartment();
+                        // If the username and password are valid then populate department list to choose from                         
+                        getDepartmentList();
                     }
                     else {
                         acceptPassword();
-                    };
+                        return true;
+                    }
                 }
             )
             return true;
         })
     });
-
-
 }
+
 function isPasswordValid(dbPassword, password) {
     console.log('in isPasswordValid : dbPassword : password : ', dbPassword, password);
     if (dbPassword != password) {
         return false;
     } else {
-        guserVerified=true;
+        guserVerified = true;
         console.log('passwords match');
         return true;
     }
 }
 
-// user prompts
-
-// show department options
-// show item options
-// show options for sizes
-
-function acceptDepartment() {
-    console.log('in accept department');
+// populate the global array to hold department names
+function getDepartmentList() {
     //use the connection to select department names
     gConnection.query('SELECT distinct department_name from products', function (error, res) {
-        console.log('After select ',res[0].department_name);
         if (error) throw error;
-        console.log(res[1]);
+
         for (var i = 0; i < res.length; i++) {
             departmentList.push(res[i].department_name);
         }
-    })
-    inquirer
-        .prompt({
-            name: "department",
-            type: "list",
-            message: "Please select the department to shop from:",
-            choices: departmentList//["Womens Dresses", "Skirts", "Ments T-shirts", "Ments Belts"]
-        })
-        .then(function (answer) {
-            // based on their answer, show the list of items to choose from 
-            console.log(answer);
-        });
+        acceptDepartment();
+    });
+}
+
+function acceptDepartment() {
+    // Chekc if the departmentList array is populated 
+    if (departmentList.length > 0) {
+        inquirer
+            .prompt({
+                name: "department",
+                type: "list",
+                message: "Please select the department to shop from:",
+                choices: departmentList
+            })
+            .then(function (answer) {
+                // based on their answer, show the list of items to choose from 
+                console.log(answer.department);
+                acceptItem(answer.department);
+
+                return true;
+            });
+    }
     return true;
+}
+
+
+
+function acceptItem(pDepartment){
+    // Check how many types of products are avaialble in the 
+    gConnection.query('select count(distinct product_name) productCnt from products where department_name =?',
+        [pDepartment],
+        function (error, res) {
+            if (error) throw error;
+            console.log(res[0].productCnt);
+             if(res[0].productCnt > 1) {
+                 console.log('more than one');
+                 // call Accept Item name
+                 gConnection.query('select distinct product_name from products where department_name =?',
+                 [pDepartment],
+                 function(error, res){
+                     if (error) throw error;
+                     console.log(res);
+                     for(var i=0; i<res.length; i++){
+                         productArray.push(res[i].product_name);
+                     }
+                     // Accept item
+                     inquirer
+                        .prompt({
+                            name: "product_name",
+                            type: "list",
+                            message: "Please select the item from the list :",
+                            choices: productArray
+                        })
+                        .then(function (answer) {
+                            // based on their answer, show the list of items to choose from 
+                            console.log(answer.product_name);
+                            populateSizeArray(pDepartment, answer.product_name);
+                            return true;
+                        });
+
+                 });
+
+             }else{
+                // get the name of single item in the department
+                gConnection.query('select distinct product_name from products where department_name =?',
+                                [pDepartment],
+                                function(error, res){
+                                    if (error) throw error;
+                                    console.log(res);
+                                    populateSizeArray(pDepartment, res[0].product_name);
+                                });
+                }
+        }) 
+}
+
+function populateSizeArray(pDepartment, pItem){
+    console.log('In populateSizeArray - pDepartment, pItem : '+pDepartment, pItem);
+    
+    gConnection.query('select size from products where department_name = ? and product_name = ?',[pDepartment,pItem],
+    function (error, res) {
+        if (error) throw error;
+        console.log(res[0].size);
+        for(var i =0;i<res.length;i++){
+            sizeArray.push(res[i].size);
+        }
+        acceptSize(pDepartment, pItem); 
+    })
+};
+
+function acceptSize(pDepartment, pItem){
+    inquirer
+            .prompt({
+                name: "size",
+                type: "list",
+                message: "Please select size :",
+                choices: sizeArray
+            })
+            .then(function (answer) {
+                // based on their answer, show the list of items to choose from 
+                console.log(answer.size);
+                acceptQuantity(pDepartment, pItem, answer.size);
+                return true;
+            });
+}
+
+function acceptQuantity(pDepartment, pItem, pSize){
+    inquirer
+            .prompt({
+                name: "quantity",
+                type: "input",
+                message: "Please enter quantity :"
+            })
+            .then(function (answer) {
+                // based on their answer, show the list of items to choose from 
+                console.log(answer.quantity);
+                var query = ' select stock_quantity quantity from products where department_name = "'+pDepartment+'" and product_name = "'+pItem+'" and size = "'+pSize+'"';
+
+                gConnection.query(query, 
+                function(error, res){
+                    if (error) throw error;
+                    if(answer.quantity <= res[0].quantity){
+                        console.log('Great! Let me get that item for you.');
+                        updateQuantity(pDepartment, pItem, pSize, answer.quantity, 'DEDUCT');
+                    }else {
+                        console.log('not enough quantity ... let me see what I can do');
+                        updateQuantity(pDepartment, pItem, pSize, answer.quantity,'ADD');
+                    }
+                    return true;
+                })
+            });
+}
+
+function updateQuantity(pDepartment, pItem, pSize, pQuantity, action){
+    var where = ' where department_name = "'+pDepartment+'" and product_name = "'+pItem+'" and size = "'+pSize+'"';
+    var query;
+    
+    if(action === 'DEDUCT'){
+        var query = 'UPDATE products set stock_quantity = stock_quantity - '+pQuantity;   
+    }else{
+        var query = 'UPDATE products set stock_quantity = stock_quantity + '+pQuantity;
+    }
+    
+    query = query + where;
+
+    gConnection.query(query ,  
+         function(error, res){
+            if (error) throw error;
+            console.log(res);
+            orderItem(pDepartment, pItem, pSize, pQuantity);                
+         });                           
+};
+
+function orderItem(pDepartment, pItem, pSize, pQuantity){
+    endConnection();
+};
+
+
+function endConnection() {
+    console.log('Thank you for shopping with us');
+    gConnection.end();
 }
 
 function start() {
     connectDB();
     acceptUserName();
-    
-    
     return true;
-
 }
 
 start();
